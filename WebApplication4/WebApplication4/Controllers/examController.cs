dc474528d76554f4fcf03168578d339167b08632
zarafsheen irfan <zaryabnalson20@gmil.com>
2022-06-08 11:14:49 -0700

    initial commit

---
+++ WebApplication4/WebApplication4/Controllers/examController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class examController : Controller
+    {
+        //
+        // GET: /exam/
+        public ActionResult xam()
+        {
+            return View();
+        }
+        public ActionResult paperdb()
+        {
+            return View();
+        }
+        public ActionResult quest()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult paperdb(paper userdata)
+        {
+           using ( online_examEntities db = new online_examEntities())
+           {
+               db.papers.Add(userdata);
+               db.SaveChanges();
+               ViewBag.Result = "inserted sucessfully";
+               ModelState.Clear();
+
+
+           }
+
+           return View(new paper());
+        }
+        public ActionResult quest(question userQ)
+        {
+            using (online_examEntities db = new online_examEntities())
+            {
+                db.questions.Add(userQ);
+                db.SaveChanges();
+                ViewBag.Result = "inserted sucessfully";
+                ModelState.Clear();
+
+
+            }
+
+            return View(new question());
+        }
+
+
+
+	}
+}
